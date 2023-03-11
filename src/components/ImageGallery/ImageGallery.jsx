64501e72e3541cf2cc4d901f0e60777d8144e674
Midland707 <rus14job@ukr.net>
2023-03-11 22:05:58 +0200

    code refactoring

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,80 +1,22 @@
-import { Component } from 'react';
+import PropTypes from 'prop-types';
+import { ImgGallery } from './ImageGallery.styled';
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
-import { Searchbar } from 'components/Searchbar/Searchbar';
-import * as ImageApi from 'components/ImageApi/ImageApi';
-import { Loader } from 'components/Loader/Loader';
-import { Button } from 'components/Button/Button';
 
-var Scroll = require('react-scroll');
-var scroll = Scroll.animateScroll;
+export const ImageGallery = ({ images, onClickImage }) => {
+  return (
+    <ImgGallery>
+      <ImageGalleryItem images={images} onClickImage={onClickImage} />
+    </ImgGallery>
+  );
+};
 
-export class ImageGallery extends Component {
-  state = {
-    query: '',
-    page: 1,
-    images: [],
-    showBtn: false,
-    isLoading: false,
-    error: '',
-  };
-
-  componentDidUpdate(prevProps, prevState) {
-    const { query, page } = this.state;
-    if (query !== prevState.query || page !== prevState.page) {
-      this.setState({ isLoading: true });
-      ImageApi.getImages(query, page)
-        .then(data => {
-          if (!data.hits.length) {
-            this.setState({
-              showBtn: false,
-            });
-            return;
-          }
-          console.log('data =', data);
-          this.setState(prevState => ({
-            images: [...prevState.images, ...data.hits],
-            showBtn: true,
-          }));
-          if (data.totalHits <= this.state.images.length + 12)
-            this.setState({
-              showBtn: false,
-            });
-        })
-        .catch(error => {
-          this.setState({
-            error: error.message,
-          });
-        })
-        .finally(
-          setTimeout(() => {
-            this.setState({ isLoading: false });
-          }, 500)
-        );
-    }
-  }
-
-  onSubmitForm = word => {
-    this.setState({
-      query: word,
-      images: [],
-    });
-  };
-
-  onClickButton = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
-    scroll.scrollToBottom();
-  };
-
-  render() {
-    return (
-      <>
-        <Searchbar onSubmitForm={this.onSubmitForm} />
-        <ImageGalleryItem images={this.state.images} />
-        {this.state.isLoading && <Loader />}
-        {this.state.showBtn && <Button onClickButton={this.onClickButton} />}
-      </>
-    );
-  }
-}
+ImageGallery.propTypes = {
+  images: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      webformatURL: PropTypes.string.isRequired,
+      tags: PropTypes.string.isRequired,
+    }).isRequired
+  ).isRequired,
+  onClickImage: PropTypes.func.isRequired,
+};
--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,60 +1,29 @@
-import React, { Component } from 'react';
-import { Modal } from 'components/Modal/Modal';
-import {
-  ImgGallery,
-  ImgGalleryItem,
-  ImgGalleryItemImage,
-} from './ImageGalleryItem.styled';
+import PropTypes from 'prop-types';
+import { ImgGalleryItem, ImgGalleryItemImage } from './ImageGalleryItem.styled';
 
-export class ImageGalleryItem extends Component {
-  state = {
-    currentImage: '',
-    showModal: false,
-    tag: '',
-  };
-
-  onClickClose = () => {
-    this.setState({
-      showModal: false,
-    });
-  };
-  componentDidUpdate(prevProps, prevState) {
-    document.addEventListener('keydown', this.onKeyEscPress, false);
-  }
-  onKeyEscPress = event => {
-    if (event.keyCode === 27)
-      this.setState({
-        showModal: false,
-      });
-  };
-
-  render() {
-    const { images } = this.props;
-    return (
-      <ImgGallery>
-        {images.map(({ id, webformatURL, tags, largeImageURL }) => (
-          <ImgGalleryItem key={id}>
-            <ImgGalleryItemImage
-              onClick={e => {
-                this.setState({
-                  showModal: true,
-                  currentImage: largeImageURL,
-                  tag: tags,
-                });
-              }}
-              src={webformatURL}
-              alt={tags}
-            />
-          </ImgGalleryItem>
-        ))}
-        {this.state.showModal && (
-          <Modal
-            image={this.state.currentImage}
-            tags={this.state.tag}
-            onClickClose={this.onClickClose}
+export const ImageGalleryItem = ({ images, onClickImage }) => {
+  return (
+    <>
+      {images.map(({ id, webformatURL, tags }, index) => (
+        <ImgGalleryItem key={id}>
+          <ImgGalleryItemImage
+            onClick={() => onClickImage(images[index])}
+            src={webformatURL}
+            alt={tags}
           />
-        )}
-      </ImgGallery>
-    );
-  }
-}
+        </ImgGalleryItem>
+      ))}
+    </>
+  );
+};
+
+ImageGalleryItem.propTypes = {
+  images: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      webformatURL: PropTypes.string.isRequired,
+      tags: PropTypes.string.isRequired,
+    }).isRequired
+  ).isRequired,
+  onClickImage: PropTypes.func.isRequired,
+};
